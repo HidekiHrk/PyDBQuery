835491e97e8de4b56ef8ff66c7ecd692dced73f8
HidekiHrk <hidekihiroki123@gmail.com>
2019-01-21 09:00:45 -0200

    .

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name='PyDBQuery',
+  packages = ['pydbquery'],
+  version = '0.1dev',
+  license='GPL-3.0',
+  description = 'Functional query selector for databases.',
+  author = 'HidekiHrk',
+  author_email = 'hidekihiroki123@gmail.com',
+  url = 'https://github.com/HidekiHrk/PyDBQuery',
+  download_url = 'https://github.com/HidekiHrk/PyDBQuery/archive/v_0.1dev.tar.gz',
+  keywords = ['python', 'db', 'database', 'better', 'query'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
